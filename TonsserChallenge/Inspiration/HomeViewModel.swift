07716d279b4bf3207e0d96ff5f11eabd40ca9339
radibarq <radi.barq@harri.com>
2020-08-03 14:35:21 +0300

    initial commit

---
+++ TonsserChallenge/Inspiration/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  TonsserChallenge
+//
+//  Created by Kristiyan Dobrev on 03/07/2020.
+//  Copyright © 2020 Tonsser. All rights reserved.
+//
+
+import Foundation
+
+struct HomeViewModel {
+    var title = "Hello World"
+}
